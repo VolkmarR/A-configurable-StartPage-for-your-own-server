29952b74a7d9a959b7dd77a69bfc38f0fe4aa650
Volkmar Rigo <volkmar@rigo.bz>
2022-05-01 12:53:27 +0200

    added Background

--- assets/app.js
+++ assets/app.js
@@ -2,12 +2,16 @@
 
 function init() {
     renderTemplate("header");
-     renderTemplate("links");
+    renderTemplate("links");
+    renderBackground();
 }
 
-function renderTemplate(name)
-{
+function renderTemplate(name) {
     const templateSouce = document.getElementById(name + '-template').innerHTML;
     const template = Handlebars.compile(templateSouce);
     document.getElementById(name).innerHTML = template(config);
+}
+
+function renderBackground() {
+    document.body.style.backgroundImage = "url('background/1.jpg')";
 }
