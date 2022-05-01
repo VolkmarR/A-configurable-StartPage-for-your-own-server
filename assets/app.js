f164927444b830af63e03f313f3b5f3430fa5b25
Volkmar Rigo <volkmar@rigo.bz>
2022-05-01 15:22:37 +0200

    Refactored Config

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,49 @@
 'use strict';
 
+var configs = {};
+
+function RegisterConfig(key, config) {
+    configs[key] = config;
+}
+
+function BuildConfig() {
+    return {
+        header: { title: "Hallo" },
+        groups: [],
+
+        setHeader: function (title) {
+            this.header.title = title;
+            return this;
+        },
+        addGroup: function (group) {
+            this.groups.push(group);
+            return this;
+        }
+
+    };
+}
+
+function BuildGroup(title) {
+    return {
+        title: title,
+        links: [],
+
+        addLink: function (name, url, target = "_Blank") {
+            this.links.push({ name: name, url: url, target: target });
+            return this;
+        }
+    }
+}
+
 function init() {
-    renderTemplate("header");
-    renderTemplate("links");
+    let config = GetConfig("default");
+
+    renderTemplate("header", config);
+    renderTemplate("links", config);
     renderBackground();
 }
 
-function renderTemplate(name) {
+function renderTemplate(name, config) {
     const templateSouce = document.getElementById(name + '-template').innerHTML;
     const template = Handlebars.compile(templateSouce);
     document.getElementById(name).innerHTML = template(config);
@@ -14,4 +51,28 @@ function renderTemplate(name) {
 
 function renderBackground() {
     document.body.style.backgroundImage = "url('background/1.jpg')";
+}
+
+function GetConfig(key) {
+    let config = configs[key];
+
+    if (config != undefined)
+        return config;
+
+    return BuildConfig()
+        .setHeader("Example Config")
+        .addGroup(
+            BuildGroup("Social")
+                .addLink("Facebook", "https://www.facebook.com")
+                .addLink("Twitter", "https://www.twitter.com")
+        )
+        .addGroup(
+            BuildGroup("Search")
+                .addLink("Google", "https://google.com")
+                .addLink("DuckDuckGo", "https://DuckDuckGo.com")
+        )
+        .addGroup(
+            BuildGroup("Git")
+                .addLink("Github", "https://github.com")
+        );
 }
--- config.js
+++ config.js
@@ -1,50 +1,18 @@
-var config = {
-    "header": {
-        "title": "Hello"
-    },
-    "bookmarks": [
-        {
-            "category": "Communicate",
-            "links": [
-                {
-                    "name": "Discord",
-                    "url": "https://discord.com"
-                },
-                {
-                    "name": "Gmail",
-                    "url": "http://gmail.com"
-                }
-            ]
-        },
-        {
-            "category": "Cloud",
-            "links": [
-                {
-                    "name": "Box",
-                    "url": "https://box.com"
-                },
-                {
-                    "name": "Dropbox",
-                    "url": "https://dropbox.com"
-                },
-                {
-                    "name": "Drive",
-                    "url": "https://drive.google.com"
-                }
-            ]
-        },
-        {
-            "category": "dummy",
-            "links": [
-                {
-                    "name": "Box",
-                    "url": "https://box.com"
-                },
-                {
-                    "name": "Dropbox",
-                    "url": "https://dropbox.com"
-                }
-            ]
-        }
-    ]
-}
+/*
+
+// Example for config registration
+
+RegisterConfig("default", BuildConfig()
+    .setHeader("Hallo")
+    .addGroup(
+        BuildGroup("first group")
+            .addLink("first", "https://example.com")
+            .addLink("second", "https://example.com")
+    )
+    .addGroup(
+        BuildGroup("first group")
+            .addLink("first", "https://example.com")
+            .addLink("second", "https://example.com")
+    )
+);
+*/
